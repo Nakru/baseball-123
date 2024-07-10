11b99ccb0555d0ab059d185bd74854bf733f4824
sanghyung.kim <sh03.kim@samsung.com>
2024-07-10 14:39:47 +0900

    [feature] 3

--- baseball_kata.py
+++ baseball_kata.py
@@ -1,6 +1,12 @@
+import re
 class Game:
     def guess(self, guess_number):
         if guess_number is None:
             raise TypeError()
+
         if len(guess_number) != 3:
+            raise TypeError()
+
+        pattern = r'^\d{3}$'
+        if not re.match(pattern, guess_number):
             raise  TypeError()
