e3a9f3e04b2dd16b51f1d2275ee112df943f49ae
sanghyung.kim <sh03.kim@samsung.com>
2024-07-10 14:40:31 +0900

    [refactoring] 3

--- baseball_kata.py
+++ baseball_kata.py
@@ -1,4 +1,6 @@
 import re
+
+
 class Game:
     def guess(self, guess_number):
         if guess_number is None:
@@ -7,6 +9,11 @@ class Game:
         if len(guess_number) != 3:
             raise TypeError()
 
+        if not self.is_three_digit_number(guess_number):
+            raise TypeError()
+
+    def is_three_digit_number(self, s):
         pattern = r'^\d{3}$'
-        if not re.match(pattern, guess_number):
-            raise  TypeError()
+        if re.match(pattern, s):
+            return True
+        return False
